ecbfb87e11739ac53489d5cb94ad8f496f70bb17
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-12-28 18:24:57 +0100

    wezterm font change

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font_with_fallback({
-	"JetBrainsMono Nerd Font",
+	"Source Code Pro",
 	"SF Mono",
 })
 
