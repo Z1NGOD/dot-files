bf359520c98285696373d64c086d113715a9b093
Z1NGOD <YaroslavBorovskiy@gmail.com>
2025-01-14 14:32:32 +0100

    remove supermaven and ditch luasnip

--- nvim/lua/z1ngod/plugins/blink.lua
+++ nvim/lua/z1ngod/plugins/blink.lua
@@ -1,6 +1,6 @@
 return {
   "saghen/blink.cmp",
-  dependencies = { "L3MON4D3/LuaSnip", version = "*" },
+  dependencies = { "rafamadriz/friendly-snippets" },
   version = "*",
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
@@ -39,21 +39,7 @@ return {
 
     signature = { enabled = true, window = { border = "rounded" } },
 
-    snippets = {
-      preset = "luasnip",
-      expand = function(snippet)
-        require("luasnip").lsp_expand(snippet)
-      end,
-      active = function(filter)
-        if filter and filter.direction then
-          return require("luasnip").jumpable(filter.direction)
-        end
-        return require("luasnip").in_snippet()
-      end,
-      jump = function(direction)
-        require("luasnip").jump(direction)
-      end,
-    },
+    snippets = { preset = "default" },
 
     sources = {
       default = { "lazydev", "lsp", "path", "snippets", "buffer" },
--- nvim/lua/z1ngod/plugins/luasnip.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "L3MON4D3/LuaSnip",
-  version = "*",
-  build = "make install_jsregexp",
-  dependencies = { "rafamadriz/friendly-snippets" },
-  config = function()
-    require("luasnip.loaders.from_vscode").lazy_load("friendly-snippets")
-  end,
-}
